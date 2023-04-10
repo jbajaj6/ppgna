2a0db8445db6a3f20c0c2a1e66ec9835b4e4d0d8
jbajaj6 <47230046+jbajaj6@users.noreply.github.com>
2023-04-09 23:46:56 -0400

    Delete puttputt directory

--- puttputt/Friction.py
+++
@@ -1,6 +0,0 @@
-class Friction:
-    def __init__(self):
-        self.coeff = -0.015
-
-    def get(self, vel):
-        return vel*self.coeff
--- puttputt/test.py
+++
@@ -1,8 +0,0 @@
-def sum_list(x, n):
-    lst = []
-    for i in range(x):
-        lst.append(n)
-        n -= 1
-    return lst
-
-print(sum_list(20, 1000))
